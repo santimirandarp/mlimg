a3b8dcaa44d9d92133b962ac441eb5e718d373cb
Santi <santimirandarp@gmail.com>
2021-01-12 12:09:01 +0000

    main stuff done

--- 3_train.py
+++ 3_train.py
@@ -6,15 +6,11 @@ from PIL import Image
 #from scipy import ndimage
 from load_datasets import load_datasets
 
+# load all datasets
 train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_datasets()
 
 
-print(classes[0])
-index = 27
-plt.imshow(train_set_x_orig[index]) 
-plt.show()
-print ("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf8")+  "' picture.")
-
+# extract images
 m_train = train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
 num_px = train_set_x_orig.shape[1]
@@ -56,10 +52,8 @@ def initialize_with_zeros(dim):
     b -- initialized scalar (corresponds to the bias)
     """
     
-    ### START CODE HERE ### (≈ 1 line of code)
     w = np.zeros((dim,1))
     b = float(0)
-    ### END CODE HERE ###
 
     assert(w.shape == (dim, 1))
     assert(isinstance(b, float) or isinstance(b, int))
@@ -89,25 +83,21 @@ def propagate(w, b, X, Y):
     m = X.shape[1] # 209 
     
     # FORWARD PROPAGATION (FROM X TO COST)
-    ### START CODE HERE ### (≈ 2 lines of code)
     # w is a column. We need to traspose it
     A = sigmoid(np.dot(w.T, X) + b) # compute activation / sigmoid , row
     cost = -(1/m)*np.sum(Y*np.log(A) + (1-Y)*np.log(1-A)) # compute cost
     ### END CODE HERE ###
     
     # BACKWARD PROPAGATION (TO FIND GRAD)
-    ### START CODE HERE ### (≈ 2 lines of code)
     dw = 1/m*np.dot(X,(A-Y).T)
     db = 1/m*np.sum(A-Y)
-    ### END CODE HERE ###
 
     assert(dw.shape == w.shape)
     assert(db.dtype == float)
     cost = np.squeeze(cost)
     assert(cost.shape == ())
     
-    grads = {"dw": dw,
-             "db": db}
+    grads = {"dw": dw, "db": db}
     
     return grads, cost
 
@@ -150,11 +140,8 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
         dw = grads["dw"]
         db = grads["db"]
         
-        # update rule (≈ 2 lines of code)
-        ### START CODE HERE ###
         w -= learning_rate*dw 
         b -= learning_rate*db
-        ### END CODE HERE ###
         
         # Record the costs
         if i % 100 == 0:
@@ -164,11 +151,9 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
         if print_cost and i % 100 == 0:
             print ("Cost after iteration %i: %f" %(i, cost))
     
-    params = {"w": w,
-              "b": b}
+    params = {"w": w, "b": b}
     
-    grads = {"dw": dw,
-             "db": db}
+    grads = {"dw": dw, "db": db}
     
     return params, grads, costs
 
@@ -191,9 +176,8 @@ def predict(w, b, X):
     w = w.reshape(X.shape[0], 1)
     
     # Compute vector "A" predicting the probabilities of a cat being present in the picture
-    ### START CODE HERE ### (≈ 1 line of code)
     A = sigmoid(np.dot(w.T,X) + b)
-    ### END CODE HERE ###
+
     print(A)
     for i in range(A.shape[1]):
         
@@ -226,8 +210,6 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 100, learning_rate 
     d -- dictionary containing information about the model.
     """
     
-    ### START CODE HERE ###
-    
     # initialize parameters with zeros (≈ 1 line of code)
     w, b = initialize_with_zeros(X_train.shape[0])
 
@@ -242,7 +224,6 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 100, learning_rate 
     Y_prediction_test = predict(w,b, X_test)
     Y_prediction_train = predict(w,b, X_train)
 
-    ### END CODE HERE ###
 
     # Print train/test Errors
     print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
@@ -259,17 +240,19 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations = 100, learning_rate 
     
     return d
 
-d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+# run
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.001, print_cost = True)
 
+# test
 index = 2
 plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
 print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" picture.")
 
 
 # Plot learning curve (with costs)
-costs = np.squeeze(d['costs'])
-plt.plot(costs)
-plt.ylabel('cost')
-plt.xlabel('iterations (per hundreds)')
-plt.title("Learning rate =" + str(d["learning_rate"]))
-plt.show()
+#costs = np.squeeze(d['costs'])
+#plt.plot(costs)
+#plt.ylabel('cost')
+#plt.xlabel('iterations (per hundreds)')
+#plt.title("Learning rate =" + str(d["learning_rate"]))
+#plt.show()
